b16a2f7501e08c205f77122f16207b15b18d2d21
Brent Thomas <brentthomas@Brents-MacBook-Air.local>
2017-03-14 16:15:36 -0600

    updated

--- Pages/UK-Pages/BBC/bbc.js
+++ Pages/UK-Pages/BBC/bbc.js
@@ -19,21 +19,41 @@ $(document).ready(function(){
 //     }
 
     var url = "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=edd5090deb6b47eaa1aa6368b9779ebe"
+  var rssArray = []
 
         $.get(url).then(function(apData) {
           console.log(apData.articles[0].title)
           for (var i =0; i < apData.articles.length; i++){
+
             var  apTitles = apData.articles[i].title
             var  apLinks = apData.articles[i].url
               console.log(apTitles)
-              var rss = (apTitles + apLinks)
-
+              $('.apTitles').addClass('titles')
+              var rss = apTitles + apLinks
+              rssArray.push(rss)
           }
-              return rss
         }).then(function(rss) {
-            $(".bbc2").html(rss)
+            $(".bbc2").contents().append(rssArray)
             console.log('bishicles')
         })
+
+        // var url = "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=edd5090deb6b47eaa1aa6368b9779ebe"
+        // var counter = 6
+        //     $.get(url).then(function(apData) {
+        //       console.log(apData.articles[0].title)
+        //       for (var i =0; i < counter; i++){
+        //         var rssArray = []
+        //         var  apTitles = apData.articles[i].titl
+        //         e
+        //         var  apLinks = apData.articles[i].url
+        //           console.log(apTitles)
+        //         var rss = apTitles + apLinks
+        //           ('rss').push(rssArray)
+        //       }
+        //     }).then(function(rss) {
+        //         $(".bbc2").html(rss)
+        //         console.log('bishicles')
+        //     })
 
 
 
